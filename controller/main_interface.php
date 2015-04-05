0e961a5add3254e6cae4fec1b7cfbd1f585ebf0c
Matt Friedman <maf675@gmail.com>
2015-04-05 14:25:48 -0700

    Add some interface for design guidance

---
+++ controller/main_interface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *
+ * Collapsible Categories extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace phpbb\collapsiblecategories\controller;
+
+/**
+ * Interface main_interface
+ * This describes all of the methods for the main controller
+ *
+ * @package phpbb\collapsiblecategories\controller
+ */
+interface main_interface
+{
+	/**
+	 * This method processes AJAX requests for collapsible categories
+	 * when a user collapses/expands a category. Collapsed categories
+	 * will be stored to the user's db and cookie. Expanded categories
+	 * will be removed from the user's db and cookie.
+	 *
+	 * @param int $forum_id A forum identifier
+	 *
+	 * @throws \phpbb\exception\http_exception An http exception
+	 * @return \Symfony\Component\HttpFoundation\JsonResponse A Symfony JSON Response object
+	 * @access public
+	 */
+	public function handle($forum_id);
+}
