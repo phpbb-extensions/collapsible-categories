347ed6ed044b122f6cb28bbc13852bc781c211c8
Raphaël M <Galixte@users.noreply.github.com>
2017-05-16 23:57:52 +0200

    Update of the FRENCH translation for the Collapsible Categories extension

--- language/fr/collapsiblecategories.php
+++ language/fr/collapsiblecategories.php
@@ -39,5 +39,5 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'COLLAPSIBLE_CATEGORIES_TITLE'		=> 'Afficher / masquer l’affichage de cette catégorie du forum',
+	'COLLAPSIBLE_CATEGORIES_TITLE'		=> 'Afficher / masquer le contenu de cette catégorie',
 ));
